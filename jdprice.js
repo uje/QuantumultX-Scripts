4361b327be1a635ddf0ce75dee79d99e8f8fdf14
Yuanhe <4176641+uje@users.noreply.github.com>
2022-07-20 21:16:53 +0800

    Create jdprice.js

---
+++ jdprice.js
@@ -0,0 +1,7 @@
+# 不生效或失效的检查一下配置有没有这两条复写，删除试试
+# ^https?:\/\/api\.m\.jd.com\/client\.action\?functionId=start - reject
+# ^https?:\/\/api\.m\.jd.com\/client\.action\?functionId=(start|queryMaterialAdverts) - reject
+[Script]
+http-response ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) requires-body=1,script-path=https://raw.githubusercontent.com/yichahucha/surge/master/jd_price.js
+[MITM]
+hostname = api.m.jd.com
